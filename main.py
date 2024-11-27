79da62c9f34fc3e50e36e4b3cccca5e3261b7b2a
Marcelo Diaz <marcelodiaz558@hotmail.com>
2024-11-26 23:21:37 -0400

    Fixed Security ImportError on main module

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from document_converter.route import router as document_converter_router
 from document_converter.auth import get_api_key
-from fastapi.security import Security
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -23,5 +22,5 @@ app.include_router(
     document_converter_router,
     prefix="",
     tags=["document-converter"],
-    dependencies=[Security(get_api_key)]
+    dependencies=[Depends(get_api_key)]
 )
